cdacce775d05fee6731563143ed3a9976ce8d097
NIKHIL KUMAR REDDY <131607109+nikhilkumarreddy29@users.noreply.github.com>
2024-03-21 06:19:38 +0000

    added Html and CSS
    added Html and CSS

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 app=Flask(__name__)
 JOBS=[
   {
@@ -32,6 +32,6 @@ def hello_nikhil():
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
-if __name__=='__main__':
+if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
   
