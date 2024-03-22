c019cfbc6971460d32e62768571de5afbbd50dde
NIKHIL KUMAR REDDY <131607109+nikhilkumarreddy29@users.noreply.github.com>
2024-03-22 13:59:16 +0000

    added dynamic data and api route
    added dynamic data and api route

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask, render_template,jsonify
 app=Flask(__name__)
 JOBS=[
   {
-  'id':1,
-  'title': 'Data Analyst',
-  'location': 'Bengaluru, India',
-  'salary': 'Rs. 10,00,000'
+   'id':1,
+   'title': 'Data Analyst',
+   'location': 'Bengaluru, India',
+   'salary': 'Rs. 10,00,000'
   },
   {
-  'id':2,
-  'title': 'Data Scientist',
-  'location': 'Delhi, India',
-  'salary': 'Rs. 15,00,000'
+   'id':2,
+   'title': 'Data Scientist',
+   'location': 'Delhi, India',
+   'salary': 'Rs. 15,00,000'
   },
   {
     'id':3,
